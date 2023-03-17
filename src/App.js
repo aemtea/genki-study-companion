755688758d04e0e7f8171eef4e5a504191c6bff4
Alex Thompson <Alex.Thompson@veteransunited.com>
2023-03-17 16:41:14 -0500

    add basic home page with lessons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import LessonSelector from './lessonSelector';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Genki Study Companion</h1>
+        <p>A study companion for practicing grammar/vocabulary introduced in Genki: An Integrated Course in Elementary Japanese.</p>
+
+        <LessonSelector />
+        <button type='button'>Study</button>
       </header>
     </div>
   );
